1747f4fa41ed355f7eb34a556f65bd26a25b30d0
Harshit Bhatt <043.harshitbhatt@gmail.com>
2023-09-24 21:32:47 +0530

    Updated script

--- curtana-docker/compile.sh
+++ curtana-docker/compile.sh
@@ -6,5 +6,5 @@ mkdir -p "$KERNEL_DIR"
 mkdir -p "$PACKAGES_DIR"
 cd "$KERNEL_DIR"
 git clone --depth 1 https://github.com/HarshitBhatt043/curtana.git
-mv curtana/* "$KERNEL_DIR"
+mv -f curtana/{.,}* "$KERNEL_DIR"
 docker run --rm -v "$PACKAGES_DIR:/buildd" -v "$KERNEL_DIR:/buildd/sources" -it quay.io/droidian/build-essential:trixie-amd64 /bin/bash /buildd/sources/curtana-docker/kernel.sh
